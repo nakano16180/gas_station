2a0ac94dd281bf19b5b21bbac409f273ef7c6e2f
nakano16180 <nakano16180@gmail.com>
2024-03-30 19:35:47 +0900

    create function to list files in folder

--- csv_import/src/example-module.ts
+++ csv_import/src/example-module.ts
@@ -1,4 +1,19 @@
 /**
+ * Copyright 2023 nakano16180
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
  * Copyright 2023 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,4 +30,18 @@
  */
 export function hello() {
   return 'Hello Apps Script!';
+}
+
+export function getFiles() {
+  // TODO: フォルダのIDを環境変数でセットするかスプシから読み取る
+  const folderId = '';
+  // ファイル一覧取得
+  const folder = DriveApp.getFolderById(folderId);
+  const files = folder.getFiles();
+
+  while (files.hasNext()) {
+    const file = files.next();
+    const fileName = file.getName();
+    console.log(fileName);
+  }
 }
